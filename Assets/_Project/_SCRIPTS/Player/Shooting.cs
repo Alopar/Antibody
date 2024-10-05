2260f5e736fb75d24e6cd1557f56101f1ea56376
Alex Lopar <alopar86@gmail.com>
2024-10-05 17:07:27 +0300

    switch weapon

--- Assets/_Project/_SCRIPTS/Player/Shooting.cs
+++ Assets/_Project/_SCRIPTS/Player/Shooting.cs
@@ -1,3 +1,5 @@
+using System;
+using TMPro;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
@@ -7,21 +9,30 @@ namespace Gameplay
     {
         #region FIELDS INSPECTOR
         [SerializeField, Range(0, 10)] private float _shootCooldown = 0.5f;
+
+        [Space(10)]
         [SerializeField] private Transform _shootPoint;
         [SerializeField] private Marker _projectilePrefab;
-        
+
         [Space(10)]
         [SerializeField] private InputActionReference _shootAction;
-        
+        [SerializeField] private InputActionReference _switchWeaponAction;
+
         [Space(10)]
-        [SerializeField] private MarkType _markType;
+        [SerializeField] private TextMeshPro _markerText;
         #endregion
 
         #region FIELDS PRIVATE
         private float _cooldownTimer;
+        private MarkType _markType;
         #endregion
 
         #region METHODS PRIVATE
+        private void Init()
+        {
+            _markerText.text = _markType.ToString();
+        }
+
         private void Shoot()
         {
             if (_cooldownTimer > Time.time) return;
@@ -31,12 +42,27 @@ namespace Gameplay
             var projectile = Instantiate(_projectilePrefab, _shootPoint.position, _shootPoint.rotation);
             projectile.SetMark(_markType);
         }
+
+        private void SwitchMarker()
+        {
+            if (!_switchWeaponAction.action.triggered) return;
+
+            _markType += 1;
+            _markType = _markType == MarkType.None ? MarkType.X : _markType;
+            _markerText.text = _markType.ToString();
+        }
         #endregion
 
         #region UNITY CALLBACKS
+        private void Awake()
+        {
+            Init();
+        }
+
         private void Update()
         {
             Shoot();
+            SwitchMarker();
         }
         #endregion
     }
