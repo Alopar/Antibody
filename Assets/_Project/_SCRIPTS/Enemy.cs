5afcb293dfee31f482c8155602c24f192788a054
MCVladIsLove <manukyan228008@mail.ru>
2024-10-05 13:38:30 +0300

    Simple enemy script

--- Assets/_Project/_SCRIPTS/Enemy.cs
+++ Assets/_Project/_SCRIPTS/Enemy.cs
@@ -1,19 +1,51 @@
+using System;
 using UnityEngine;
 
 namespace Gameplay
 {
     public class Enemy : MonoBehaviour
     {
-        // Start is called once before the first execution of Update after the MonoBehaviour is created
-        void Start()
+        [SerializeField] private float _health;
+        [SerializeField] private float _moveSpeed;
+        [SerializeField] private float _attackDistance;
+        [SerializeField] private float _contactOffset;
+        [SerializeField] private float _attackCooldown;
+        [SerializeField] private float _attackDamage;
+        [SerializeField] private MarkType _mark;
+
+        private PlayerTemp _player;
+        private float _timer;
+
+        private void Awake()
         {
-        
+            _player = FindAnyObjectByType<PlayerTemp>();
         }
 
-        // Update is called once per frame
-        void Update()
+        private void Update()
         {
-        
+            _timer += Time.deltaTime;
+
+            MoveToPlayer();
+
+            if (Vector3.Distance(_player.transform.position, transform.position) <= _contactOffset)
+                Attack();
+        }
+
+        private void Attack()
+        {
+            if (_timer < _attackCooldown)
+                return;
+
+            _timer = 0;
+            Debug.Log("Attacking Player");
+        }
+
+        private void MoveToPlayer()
+        {
+            if (Vector3.Distance(_player.transform.position, transform.position) <= _contactOffset)
+                return;
+
+            transform.position = Vector3.MoveTowards(transform.position, _player.transform.position, _moveSpeed * Time.deltaTime);
         }
     }
 }
